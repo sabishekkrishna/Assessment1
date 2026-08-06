7089d7025dedff060026ae32d13ffbb3aeb37fd1
sabishekkrishna <ataprobe@gmail.com>
2026-08-06 10:42:00 +0530

    Version 3

--- salary.py
+++ salary.py
@@ -3,6 +3,10 @@ def basic_pay(hours,rate):
 
 def add_hra(basic, percent):
     return (basic+basic*percent*0.01)
+
+
 if __name__=="__main__":
     ans = basic_pay(160,250)
     print(ans)
+    hra = add_hra(160,10 )
+    print(hra)
