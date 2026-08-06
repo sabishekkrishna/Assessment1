4fe4485d886b2e4ca343f506d8bf3c3a0e3b861b
sabishekkrishna <ataprobe@gmail.com>
2026-08-06 10:43:56 +0530

    Version 4

--- salary.py
+++ salary.py
@@ -4,9 +4,12 @@ def basic_pay(hours,rate):
 def add_hra(basic, percent):
     return (basic+basic*percent*0.01)
 
-
+def deduct_tax(salary, percent):
+    return (salary - salary*percent*0.01)
 if __name__=="__main__":
     ans = basic_pay(160,250)
     print(ans)
     hra = add_hra(160,10 )
     print(hra)
+    ded = deduct_tax(hra, 5)
+    print(ded)
